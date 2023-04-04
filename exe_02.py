106d6fff5cad01b7c3c74a9aff64a3efc29ee599
RAFAEL ANTUNES MALAQUIAS <126631387+rmalaquias2023@users.noreply.github.com>
2023-04-04 20:21:13 -0300

    Add files via upload

---
+++ exe_02.py
@@ -0,0 +1,67 @@
+import pytest 
+
+def cardapio (codigo, pedir_mais):
+    cardapio = ''' 
+    *******************Cardápio*******************
+    ----------------------------------------------
+    | Código |        Descrição        |  Valor  |
+    |   100  |     Cachorro Quente     |   9,00  |
+    |   101  |  Cachorro Quente Duplo  |  11,00  |
+    |   102  |           X-Egg         |  12,00  |
+    |   103  |         X-Salada        |  12,00  |
+    |   104  |          X-Bacon        |  14,00  |
+    |   105  |           X-Tudo        |  17,00  |
+    |   200  |    Refrigerente Lata    |   5,00  |
+    |   201  |         Chá Gelado      |   4,00  |
+    ----------------------------------------------
+    '''
+    print(cardapio)
+
+    total = 0.0
+    #codigo = int(input('Entre com o código desejado: '))
+
+    while True:
+        if codigo == 100:
+            print('Você pediu um Cachorro Quente no valor de 9,00')
+            total += 9
+        elif codigo == 101:
+            print('Você pediu um Cachorro Quente Duplo no valor de 11,00')
+            total += 11
+        elif codigo == 102:
+            print('Você pediu um X-Egg no valor de 12,00')
+            total += 12
+        elif codigo == 103:
+            print('Você pediu um X-Salada no valor de 12,00')
+            total += 12
+        elif codigo == 104:
+            print('Você pediu um X-Bacon no valor de 14,00')
+            total += 14
+        elif codigo == 105:
+            print('Você pediu um X-Tudo no valor de 17,00')
+            total += 17
+        elif codigo == 200:
+            print('Você pediu um Refrigerante Lata no valor de 5,00')
+            total += 5
+        elif codigo == 201:
+            print('Você pediu um Chá Gelado no valor de 4,00')
+            total += 4
+        else:
+            print('Opção inválida')
+            codigo = int(input('Entre com o código desejado: '))
+            continue
+
+        print('Deseja pedir mais alguma coisa?')
+        print('1 - Sim')
+        print('2 - Não')
+        #pedir_mais = int(input())
+
+        if pedir_mais == 2:
+            return total
+            break
+
+        #codigo = int(input('Entre com o código desejado: '))
+
+    print(f'O total a ser pago é: {total:.2f} R$')
+
+def test_cardapio ():
+    assert cardapio (100, 2) == 500.00 
